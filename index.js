dc98c8fa641e3364aa22ee31a5f3aa286403bb82
Danilo Woznica <danilowoz@gmail.com>
2018-08-31 15:09:03 +0100

    feat(Plugin): Flow (#7)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "babel-eslint",
   extends: ["airbnb", "prettier", "prettier/react"],
-  plugins: ["react", "prettier"],
+  plugins: ["react", "prettier", "flowtype"],
   rules: {
     "jsx-a11y/href-no-hash": 0,
     "react/jsx-filename-extension": "off",
@@ -17,6 +17,35 @@ module.exports = {
       {
         semi: false
       }
-    ]
+    ],
+    "flowtype/boolean-style": [2, "boolean"],
+    "flowtype/define-flow-type": 1,
+    "flowtype/delimiter-dangle": [2, "never"],
+    "flowtype/generic-spacing": [2, "never"],
+    "flowtype/no-primitive-constructor-types": 2,
+    "flowtype/no-types-missing-file-annotation": 2,
+    "flowtype/no-weak-types": 2,
+    "flowtype/object-type-delimiter": [2, "comma"],
+    "flowtype/require-parameter-type": 2,
+    "flowtype/require-return-type": [
+      0,
+      "always",
+      {
+        "annotateUndefined": "never"
+      }
+    ],
+    "flowtype/require-valid-file-annotation": 2,
+    "flowtype/space-after-type-colon": [0, "always"],
+    "flowtype/space-before-generic-bracket": [2, "never"],
+    "flowtype/space-before-type-colon": [2, "never"],
+    "flowtype/type-id-match": [2, "^([A-Z][a-z0-9]+)+Type$"],
+    "flowtype/union-intersection-spacing": [2, "always"],
+    "flowtype/use-flow-type": 1,
+    "flowtype/valid-syntax": 1
+  },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": false
+    }
   }
 };
